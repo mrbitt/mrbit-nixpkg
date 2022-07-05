d819ea7f0f0c55ad287e6fe4505eeb58df4dd819
mrbitt <giacomogiorgianni@gmail.com>
2022-07-05 16:07:09 +0200

    add themes xpenguins

--- games/xpenguins/default.nix
+++ games/xpenguins/default.nix
@@ -9,9 +9,19 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-talhaExGFAlSf+8s8mbYrjgjvXqc955nj6IF4d5hHA8=";
   };
 
+  themes = fetchurl {
+    url = "http://xpenguins.seul.org/xpenguins_themes-1.0.tar.gz";
+    sha256 = "sha256-1q7ujHsMWgKxh8vh14E0FXbXEaJfg3x14R+ZepLpqVc=";
+  };
+  
    nativeBuildInputs = [ pkg-config autoreconfHook ];
 
    buildInputs = [ libX11 libXpm libXt gtk3 ];
+ 
+  postInstall = ''
+    tar -xvzf ${themes} -C $out/share/${pname}/ 
+   '';
+  
 
   meta = with lib; {
     description = "Ever wanted cute little penguins walking along the tops of your windows?";
