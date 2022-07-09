a7938b2fa6334baeee040cc2ee5210394e454da7
mrbitt <giacomogiorgianni@gmail.com>
2022-07-09 23:59:31 +0200

    update xdiskusage 1.60

--- xdiskusage/default.nix
+++ xdiskusage/default.nix
@@ -1,16 +1,23 @@
-{stdenv, fetchgit, fltk13, autoconf}:
+{stdenv, lib, fetchgit, fltk13, autoconf}:
 
 stdenv.mkDerivation {
-  name = "xdiskusage-1.51";
+  name = "xdiskusage-1.60";
 
   src = fetchgit {
     url = "git://git.code.sf.net/p/xdiskusage/git";
-    rev = "2fa92f0544169fbf0173dfbfd519488a16b641af";
-    sha256 = "0ldqrcf250sncm6arilqf6170bqykqa1kqfd8f1wd4vxnnp29kxl";
+    rev = "d8b99e5c4026125f3d31c90a83ebf180f559a036";
+    sha256 = "sha256-mQaD/yu0vgpFluzKhT9JbkF2L2lSOgrkFK2vGl1J0Lo=";
   };
   preConfigure = ''
     autoconf
   '';
   buildInputs = [ fltk13 autoconf  ];
 
+ meta = with lib; {
+    description = "user-friendly program to show you what is using up all your disk space";
+    homepage = "http://xdiskusage.sourceforge.net/";
+    license = licenses.gpl2; 
+    maintainers = [  ];
+    platforms = platforms.linux;
+  };
 }
