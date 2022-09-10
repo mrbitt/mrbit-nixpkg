2339a47ea038093d37c2c0ee479460fe560a51a4
mrbitt <giacomogiorgianni@gmail.com>
2022-09-10 08:34:21 +0200

    gitkraken 8.9.0

--- applications/version-management/gitkraken/default.nix
+++ applications/version-management/gitkraken/default.nix
@@ -10,14 +10,14 @@ with lib;
 
 let
   pname = "gitkraken";
-  version = "8.8.0";
+  version = "8.9.0";
 
   throwSystem = throw "Unsupported system: ${stdenv.hostPlatform.system}";
 
   srcs = {
     x86_64-linux = fetchzip {
       url = "https://release.axocdn.com/linux/GitKraken-v${version}.tar.gz";
-      sha256 = "sha256-F/uqV6CL/zFERS6nKVD8kEqCZQ2O9AGGzU5uCJ94qwk=";
+      sha256 = "sha256-gycP/KCfy+FecIcNyITpOuEEeByoB8S9pKGrhpUs7sc=";
     };
 
     x86_64-darwin = fetchzip {
